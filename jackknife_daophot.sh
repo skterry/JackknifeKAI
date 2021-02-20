286f91d5c2a84e7c0297f151ba2cb77e4ee5ac28
skterry <sterry4261@gmail.com>
2021-02-20 03:40:05 -0800

    initial

--- jackknife_daophot.sh
+++ jackknife_daophot.sh
@@ -1,7 +1,7 @@
-for i in {1..8}; do  # STEP 1
-head -19 input_daophot.txt >> input_daophot.txt
-sed -i -e "$((19*i)),$((19*(i+1)))s/mag_OB06284_kp_tdOpen_1/mag_OB06284_kp_tdOpen_$((i+1))/g" input_daophot.txt
-done
+#for i in {1..8}; do  # STEP 1
+#head -19 input_daophot.txt >> input_daophot.txt
+#sed -i -e "$((19*i)),$((19*(i+1)))s/mag_OB06284_kp_tdOpen_1/mag_OB06284_kp_tdOpen_$((i+1))/g" input_daophot.txt
+#done
 
 #-----Comments-----#
 # sed -i -e 's/blabla_old.coo/new_blabla.coo/g' input_daophot.txt in case need to change .coo file
