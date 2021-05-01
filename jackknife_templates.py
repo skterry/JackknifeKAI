58f7f488956b54cb6ae5f4f13516632af4d7545f
skterry <sterry4261@gmail.com>
2021-04-30 20:17:26 -0700

    remove unnecessary underline suffix

--- jackknife_templates.py
+++ jackknife_templates.py
@@ -29,7 +29,7 @@ def jackknife():
         data.clean(jack_list, 'nite1', 'ks', refSrc1, refSrc1, instrument=nirc2)
         data.calcStrehl(jack_list, 'ks', instrument=nirc2)
         data.combine(jack_list, 'ks', '27maylgs', trim=1, weight='strehl',
-                    submaps=3, instrument=nirc2, outSuffix='_' + str(i[0]))
+                    submaps=3, instrument=nirc2, outSuffix=str(i[0]))
         os.chdir('reduce')
 
 
@@ -67,6 +67,6 @@ def jackknife():
         data.clean(jack_list, target, 'kp_tdOpen', refSrc, refSrc, field=target, instrument=osiris)
         data.calcStrehl(jack_list, 'kp_tdOpen', field=target, instrument=osiris)
         data.combine(jack_list, 'kp_tdOpen', epoch, field=target,
-                     trim=0, weight='strehl', submaps=3, instrument=osiris, outSuffix='_' + str(i[0]))
+                     trim=0, weight='strehl', submaps=3, instrument=osiris, outSuffix=str(i[0]))
         os.chdir('reduce')
 
