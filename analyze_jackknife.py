a84fb8ca43bc50691a25abb7561ac63931399bdd
skterry <sterry4261@gmail.com>
2021-05-03 22:41:18 -0700

    bug fixes

--- analyze_jackknife.py
+++ analyze_jackknife.py
@@ -18,7 +18,7 @@ id_star1 = int(372)
 id_star2 = int(117)
 id_star3 = [] #Optional 3rd(blend) star
 jack_img_num = int(14)
-dt = int(11.12)
+dt = int(11.199)
 
 #-------------------------------------------------
 #-------------------------------------------------
@@ -38,86 +38,53 @@ for i in range(jack_img_num):
     for j in data_dict[i]:
         if j[0] == id_star2:
             star2_list.append(j[0:5])
-        
+# If 3-star jackknife, uncomment loop below (and 3-star jackknife_list below):            
+#    for j in data_dict[i]:
+#        if j[0] == id_star3:
+#            star3_list.append(j[0:5])
+            
+#Stack stars in new jackknife_list
 jackknife_list = np.hstack((star1_list, star2_list))
-
 np.savetxt('jackknife_vals.dat', jackknife_list, fmt='%3s', header="id1, x1, y1, m1, me1, id2, x2, y2, m2, me2")
+#-------
+#3-star jackknife list below here:
+#jackknife_list = np.hstack((star1_list, star2_list, star3_list))
+#np.savetxt('jackknife_vals.dat', jackknife_list, fmt='%3s', header="id1, x1, y1, m1, me1, id2, x2, y2, m2, me2, id3, x3, y3, m3, me3")
 
 #Calculate means and uncertainties for x,y,m via Equation 3 in Bhattacharya et al. 2021.
-jackknife_list = np.delete(jackknife_list, (3,7), axis=0) #remove outlier and/or alt. ref image data.
-jack_img_num = 12
+
+#jackknife_list = np.delete(jackknife_list, (3,7), axis=0) #remove outlier (optional).
+#jack_img_num = 12
 #print(jackknife_list)
 
-x1bar = np.mean(jackknife_list[:, 1])
-y1bar = np.mean(jackknife_list[:, 2])
-m1bar = np.mean(jackknife_list[:, 3])
+dx = (jackknife_list[:, 6] - jackknife_list[:, 1])
+dy = (jackknife_list[:, 7] - jackknife_list[:, 2])
+dm = (jackknife_list[:, 8] - jackknife_list[:, 3])
+mtot = -2.5*np.log10(((10**(-0.4*jackknife_list[:, 3]))+(10**(-0.4*jackknife_list[:, 8]))))
+fratio = (10**(-0.4*jackknife_list[:, 8])/(10**(-0.4*jackknife_list[:, 3])))
 
-x2bar = np.mean(jackknife_list[:, 6])
-y2bar = np.mean(jackknife_list[:, 7])
-m2bar = np.mean(jackknife_list[:, 8])
+dxbar = np.mean((jackknife_list[:, 6] - jackknife_list[:, 1]))
+dybar = np.mean((jackknife_list[:, 7] - jackknife_list[:, 2]))
+dmbar = np.mean((jackknife_list[:, 8] - jackknife_list[:, 3]))
+mtotbar = np.mean(-2.5*np.log10(((10**(-0.4*jackknife_list[:, 3]))+(10**(-0.4*jackknife_list[:, 8])))))
+fratiobar = np.mean((10**(-0.4*jackknife_list[:, 8])/(10**(-0.4*jackknife_list[:, 3]))))
 
-x1err = np.sqrt(((jack_img_num - 1)/(jack_img_num))*np.sum((jackknife_list[:,1] - x1bar)**2))
-y1err = np.sqrt(((jack_img_num - 1)/(jack_img_num))*np.sum((jackknife_list[:,2] - y1bar)**2))
-m1err = np.sqrt(((jack_img_num - 1)/(jack_img_num))*np.sum((jackknife_list[:,3] - m1bar)**2))
-
-x2err = np.sqrt(((jack_img_num - 1)/(jack_img_num))*np.sum((jackknife_list[:,6] - x2bar)**2))
-y2err = np.sqrt(((jack_img_num - 1)/(jack_img_num))*np.sum((jackknife_list[:,7] - y2bar)**2))
-m2err = np.sqrt(((jack_img_num - 1)/(jack_img_num))*np.sum((jackknife_list[:,8] - m2bar)**2))
-
-sep_X = np.abs(x1bar - x2bar)
-sep_X_err = np.sqrt((x1err)**2 + (x2err)**2)
-
-sep_Y = np.abs(y1bar - y2bar)
-sep_Y_err = np.sqrt((y1err)**2 + (y2err)**2)
+dxerr = np.sqrt(((jack_img_num - 1)/(jack_img_num))*np.sum((dx - dxbar)**2))
+dyerr = np.sqrt(((jack_img_num - 1)/(jack_img_num))*np.sum((dy - dybar)**2))
+dmerr = np.sqrt(((jack_img_num - 1)/(jack_img_num))*np.sum((dm - dmbar)**2))
+mtoterr = np.sqrt(((jack_img_num - 1)/(jack_img_num))*np.sum((mtot - mtotbar)**2))
+fratioerr = np.sqrt(((jack_img_num - 1)/(jack_img_num))*np.sum((fratio - fratiobar)**2))
 
 #Print results
-print("x1 =", x1bar, "+-", x1err)
-print("y1 =", y1bar, "+-", y1err)
-print("m1 =", m1bar, "+-", m1err)
+print("dx = x1 - x2 =", dxbar, "+-", dxerr, "pix")
+print("dy = y1 - y2 =", dybar, "+-", dyerr, "pix")
 print("#-------------------------")
-print("x2 =", x2bar, "+-", x2err)
-print("y2 =", y2bar, "+-", y2err)
-print("m2 =", m2bar, "+-", m2err)
+print("mu_rel,HE =", (dxbar*9.942)/dt, "+-", (dxerr*9.942)/dt, "mas/yr")
+print("mu_rel,HN =", (dybar*9.942)/dt, "+-", (dyerr*9.942)/dt, "mas/yr")
+print("mu_rel,H =", np.sqrt((dxbar)**2 + (dybar)**2)*9.942/dt, "+-", np.sqrt((dxerr)**2 + (dyerr)**2)*9.942/dt, "mas/yr")
 print("#-------------------------")
-print("sep_X =", sep_X, "+-", sep_X_err, "pix")
-print("sep_Y =", sep_Y, "+-", sep_Y_err, "pix")
-print("#-------------------------")
-print("mu_rel,HE =", (sep_X*9.942)/dt, "+-", (sep_X_err*9.942)/dt, "mas/yr")
-print("mu_rel,HN =", (sep_Y*9.942)/dt, (sep_Y_err*9.942)/dt, "mas/yr")
-print("mu_rel,H =", np.sqrt((sep_X)**2 + (sep_Y)**2)*9.942/dt, "+-", np.sqrt((sep_X_err)**2 + (sep_Y_err)**2)*9.942/dt, "mas/yr")
-print("#-------------------------")
-print("m2 - m1 =", m2bar - m1bar, "+-", np.sqrt((m1err)**2 + (m2err)**2))
-
-#Propagate flux ratio errors (to-do: double check if propagation is correct! -ST)
-#--------------------------------
-exp_err = np.sqrt((0.4*m2err)**2 + (0.4*m1err)**2)
-ln_err = exp_err * np.log(10)
-f2f1_err = np.sqrt((0.4*m2err)**2 + (0.4*m1err)**2) * np.log(10) * (10**(-0.4*m2bar))/(((10**(-0.4*m1bar))))
-f1f2_err = np.sqrt((0.4*m1err)**2 + (0.4*m2err)**2) * np.log(10) * (10**(-0.4*m1bar))/(((10**(-0.4*m2bar))))
-
-f1tot_exp_err = np.sqrt((exp_err)**2 + (0.4*m1err)**2)
-f2tot_exp_err = np.sqrt((exp_err)**2 + (0.4*m2err)**2)
-
-f1tot_err = np.sqrt((exp_err)**2 + (0.4*m1err)**2) * np.log(10) * (10**(-0.4*m1bar))/((10**(-0.4*m1bar))+(10**(-0.4*m2bar)))
-f2tot_err = np.sqrt((exp_err)**2 + (0.4*m2err)**2) * np.log(10) * (10**(-0.4*m2bar))/((10**(-0.4*m1bar))+(10**(-0.4*m2bar)))
-#--------------------------------
-
-print("f2 / f1 =", (10**(-0.4*m2bar))/(((10**(-0.4*m1bar)))), "+-", f2f1_err)
-print("f1 / f2 =", (10**(-0.4*m1bar))/(((10**(-0.4*m2bar)))), "+-", f1f2_err)
-print("f1 / (f1+f2) =", (10**(-0.4*m1bar))/((10**(-0.4*m1bar))+(10**(-0.4*m2bar))), "+-", f1tot_err)
-print("f2 / (f1+f2) =", (10**(-0.4*m2bar))/((10**(-0.4*m1bar))+(10**(-0.4*m2bar))), "+-", f2tot_err)
-
-fig = plt.subplots(figsize=(8,8))
-
-plt.errorbar(x1bar,y1bar,xerr=x1err,yerr=y1err, color='r')
-plt.errorbar(x2bar,y2bar,xerr=x2err,yerr=y2err, color='b')
-
-plt.xlim(781,785)
-plt.ylim(427,432)
-
-plt.xlabel('X')
-plt.ylabel('Y')
-
-plt.show()
+print("dm = m1 - m2 =", dmbar, "+-", dmerr)
+print("m_total =", mtotbar, "+-", mtoterr)
+print("flux ratio = f1 / f2 =", fratiobar, "+-", fratioerr)
 
 
