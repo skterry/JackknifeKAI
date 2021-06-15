18a25c6d6be3d4cfd657ef7654bdf987e3c674ff
skterry <sterry4261@gmail.com>
2021-06-14 22:35:23 -0700

    remove submap flag

--- jackknife_templates.py
+++ jackknife_templates.py
@@ -16,20 +16,33 @@ def jackknife():
     # Ks-band reduction
     ##########
 
-    target = 'OB110950'
-    sci_files = list(range(133, 146+1))
-    sky_files = list(range(224, 233+1))
-    refSrc1 = [353., 469.] #This is the target
+    # Nite 1
+    target = 'MB07192'
+    sci_files1 = list(range(173, 177+1))
+    sky_files1 = list(range(206, 215+1))
+    refSrc1 = [385., 440.] #This is the target nearest to center
 
-    sky.makesky(sky_files, 'nite1', 'ks', instrument=nirc2)
+    sky.makesky(sky_files1, 'nite1', 'ks', instrument=nirc2)
+    data.clean(sci_files1, 'nite1', 'ks', refSrc1, refSrc1, instrument=nirc2)
+
+
+    # Nite 2
+    sci_files2 = list(range(195, 203+1))
+    sky_files2 = list(range(206, 215+1))
+    refSrc2 = [387., 443.] #This is the target nearest to center
+
+    sky.makesky(sky_files2, 'nite2', 'ks', instrument=nirc2)
+    data.clean(sci_files2, 'nite2', 'ks', refSrc2, refSrc2, instrument=nirc2)
+    #-----------------
+
+    sci_files = sci_files1 + sci_files2
 
     for i in enumerate(sci_files, start=1):
         jack_list = sci_files[:]
         jack_list.remove(i[1])
-        data.clean(jack_list, 'nite1', 'ks', refSrc1, refSrc1, instrument=nirc2)
         data.calcStrehl(jack_list, 'ks', instrument=nirc2)
         data.combine(jack_list, 'ks', '27maylgs', trim=1, weight='strehl',
-                    submaps=3, instrument=nirc2, outSuffix=str(i[0]))
+                    instrument=nirc2, outSuffix='_' + str(i[0]))
         os.chdir('reduce')
 
 
@@ -64,6 +77,6 @@ def jackknife():
         data.clean(jack_list, target, 'kp_tdOpen', refSrc, refSrc, field=target, instrument=osiris)
         data.calcStrehl(jack_list, 'kp_tdOpen', field=target, instrument=osiris)
         data.combine(jack_list, 'kp_tdOpen', epoch, field=target,
-                     trim=0, weight='strehl', submaps=3, instrument=osiris, outSuffix=str(i[0]))
+                     trim=0, weight='strehl', instrument=osiris, outSuffix=str(i[0]))
         os.chdir('reduce')
 
